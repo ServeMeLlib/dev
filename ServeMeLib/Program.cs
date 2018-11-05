9de36675ab56f48d3fa70be6553a77897f206a9a
Sam <contactsamie@gmail.com>
2018-11-04 22:48:33 -0800

    update

---
+++ ServeMeLib/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServeMeLib
+{
+    using System.Diagnostics;
+    using System.Reflection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loc = Assembly.GetEntryAssembly().Location;
+
+
+            //create server with auto assigned port
+            var myServer = new SimpleHttpServer(System.IO.Path.GetDirectoryName(loc));
+            Process.Start("http://localhost:" + myServer.Port);
+            Console.ReadKey();
+        }
+    }
+}
