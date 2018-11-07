9fa2ea63b7636a4dbb303bb032b53b7a9bb543c3
Sam <contactsamie@gmail.com>
2018-11-06 18:24:27 -0800

    trying cross platform

---
+++ ServeMeLibCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ServeMeLibCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new ServeMe())
+            {
+                server.Start();
+                Console.ForegroundColor = ConsoleColor.White;
+                do
+                {
+                    Console.WriteLine("Enter 'e' window to exit");
+                }
+                while (Console.ReadLine()?.Trim().ToLower() != "e");
+            }
+        }
+    }
+}
